55962c0411c6164dfd7b5d2525889d1d13af54d4
Pamela <pamelapatrick464@gmail.com>
2026-05-28 21:05:56 +0100

    ci: fix terraform cicd addons and platform role mapping

--- scripts/setup-github-oidc-roles.sh
+++ scripts/setup-github-oidc-roles.sh
@@ -476,25 +476,6 @@ make_foundation_apply_policy() {
         "ec2:ModifyInstancePlacement"
       ],
       "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ssm:SendCommand",
-        "ssm:GetCommandInvocation",
-        "ssm:ListCommandInvocations"
-      ],
-      "Resource": [
-        "arn:aws:ec2:${REGION}:${ACCOUNT_ID}:instance/*",
-        "arn:aws:ssm:${REGION}::document/AWS-RunShellScript"
-      ],
-      "Condition": {
-        "StringLike": {
-          "ssm:resourceTag/Name": [
-            "*-bastion"
-          ]
-        }
-      }
     }
   ]
 }
