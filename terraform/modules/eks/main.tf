83431e27d55c6d41473c126ec5b44b00330beb60
Pamela <pamelapatrick464@gmail.com>
2026-05-29 01:29:36 +0100

    add node policy for ebs-csi

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -162,6 +162,10 @@ resource "aws_iam_role_policy_attachment" "node_ecr" {
   role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+resource "aws_iam_role_policy_attachment" "node_ebs_csi" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
 
 resource "aws_iam_role_policy_attachment" "node_ssm" {
   role       = aws_iam_role.node.name
